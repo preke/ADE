5c5e9f125b7e012912d8d514eae98161152f6fa3
preke <geek00021@gmail.com>
2023-08-01 19:29:13 +0800

    with role

--- src/roberta_with_trainer.py
+++ src/roberta_with_trainer.py
@@ -106,10 +106,10 @@ def training(data, mode):
 
         training_args = TrainingArguments(
             output_dir="peft-p-tuning",
-            learning_rate=1e-3,
+            learning_rate=1e-4,
             per_device_train_batch_size=8,
             per_device_eval_batch_size=8,
-            num_train_epochs=2,
+            num_train_epochs=3,
             weight_decay=0.01,
             evaluation_strategy="epoch",
             save_strategy="epoch",
