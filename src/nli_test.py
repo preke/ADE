11d7418c28287804777317cdf4f32afd5f384456
preke <geek00021@gmail.com>
2023-08-02 19:58:07 +0800

    with role

--- src/nli_test.py
+++ src/nli_test.py
@@ -27,6 +27,19 @@ SEED = 42
 metric = evaluate.load('f1')
 
 
+class CustomTrainer(Trainer):
+    def compute_loss(self, model, inputs, return_outputs=False):
+        labels = inputs.get("labels")
+        # forward pass
+        outputs = model(**inputs)
+        logits = outputs.get("logits")
+        # compute custom loss (suppose one has 3 labels with different weights)
+        loss_fct = nn.CrossEntropyLoss()
+        loss = loss_fct(logits.view(-1, self.model.config.num_labels), labels.view(-1))
+        return (loss, outputs) if return_outputs else loss
+
+
+
 def load_data(tsv_file):
     df = pd.read_csv(tsv_file, sep='\t')
     data_path = '../data/tmp.jsonl'
@@ -124,6 +137,7 @@ def training(data, mode):
         data_collator=data_collator,
         tokenizer=tokenizer,
         compute_metrics=compute_metrics,
+        callbacks=[CustomTrainer],
     )
 
     trainer.train()
